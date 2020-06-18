5fed9f1b4feffde525a5fb99995bc4776ce16200
Sarath Dev <sarathsekaran2007@gmail.com>
2020-06-18 12:42:56 +0530

    Mail testing

---
+++ mail.php
@@ -0,0 +1,8 @@
+<?php
+$to = "sarathsekaran2007@gmail.com";
+$subject = "My subject";
+$txt = "Hello world!";
+$headers = "From: webmaster@example.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+
+mail($to,$subject,$txt,$headers);
